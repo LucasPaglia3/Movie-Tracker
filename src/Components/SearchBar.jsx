e23d63be05f886dadbc77b623857deb7d3faa721
lucaspaglia3 <lucaspaglia32@gmail.com>
2023-10-24 23:51:34 -0300

    QOL update(apreta tecla enter)

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useRef } from "react";
+
 const SearchBar = ({ query, setQuery }) => {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) {
+        return;
+      }
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+    inputEl.current.focus();
+
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
+  useEffect(() => {});
+
   return (
     <input
       className="search"
@@ -6,6 +29,7 @@ const SearchBar = ({ query, setQuery }) => {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 };
